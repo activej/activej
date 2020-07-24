f9577aed9fc6f9c1bcf2b2da1c0f7171e3e3f35e
Dmitry Volvach <dmitry@activej.io>
2020-07-24 14:46:41 +0300

    Clean up ChannelInput, ChannelOutput, perform minor refactorings

--- core-csp/src/main/java/io/activej/csp/ChannelOutput.java
+++ core-csp/src/main/java/io/activej/csp/ChannelOutput.java
@@ -16,22 +16,13 @@
 
 package io.activej.csp;
 
-import io.activej.csp.dsl.ChannelConsumerTransformer;
 import io.activej.csp.queue.ChannelQueue;
 import io.activej.csp.queue.ChannelZeroBuffer;
 import io.activej.promise.Promise;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
 @FunctionalInterface
 public interface ChannelOutput<T> {
 	void set(ChannelConsumer<T> output);
-
-	default void set(Promise<ChannelConsumer<T>> outputPromise) {
-		set(ChannelConsumer.ofPromise(outputPromise));
-	}
 
 	default ChannelSupplier<T> getSupplier() {
 		return getSupplier(new ChannelZeroBuffer<>());
@@ -40,26 +31,6 @@ public interface ChannelOutput<T> {
 	default ChannelSupplier<T> getSupplier(ChannelQueue<T> queue) {
 		set(queue.getConsumer());
 		return queue.getSupplier();
-	}
-
-	default <R> ChannelOutput<R> transformWith(ChannelConsumerTransformer<R, ChannelConsumer<T>> fn) {
-		return output -> set(output.transformWith(fn));
-	}
-
-	default <R> ChannelOutput<R> map(Function<? super T, ? extends R> fn) {
-		return output -> set(output.map(fn));
-	}
-
-	default <R> ChannelOutput<R> mapAsync(Function<? super T, ? extends Promise<R>> fn) {
-		return output -> set(output.mapAsync(fn));
-	}
-
-	default ChannelOutput<T> filter(Predicate<? super T> predicate) {
-		return output -> set(output.filter(predicate));
-	}
-
-	default ChannelOutput<T> peek(Consumer<? super T> peek) {
-		return output -> set(output.peek(peek));
 	}
 
 	default Promise<Void> bindTo(ChannelInput<T> to) {
