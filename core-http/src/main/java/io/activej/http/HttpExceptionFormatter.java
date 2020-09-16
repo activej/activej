3130d3420d7f31cb7866831986f7fd60501ae1de
Eduard Vasinskyi <evasinskyi@activej.io>
2020-09-16 16:39:37 +0300

    Bump to 3.0-SNAPSHOT

--- core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
+++ core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
@@ -28,7 +28,7 @@ import static io.activej.http.HttpHeaders.*;
  */
 @FunctionalInterface
 public interface HttpExceptionFormatter {
-	String ACTIVEJ_VERSION = "2.0-SNAPSHOT";
+	String ACTIVEJ_VERSION = "3.0-SNAPSHOT";
 
 	@Language("HTML")
 	String HTTP_ERROR_HTML = "<!doctype html>" +
