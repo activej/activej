b86ff72d0d4b02c54a5dc388ac16e42ebb86c21a
Eduard Vasinskyi <evasinskyi@datakernel.io>
2021-03-16 15:49:10 +0200

    Add SerializeSubclassesExample

---
+++ examples/core/serializer/src/main/java/SerializeSubclassesExample.java
@@ -0,0 +1,57 @@
+import io.activej.serializer.BinarySerializer;
+import io.activej.serializer.SerializerBuilder;
+import io.activej.serializer.annotations.Serialize;
+import io.activej.serializer.annotations.SerializeSubclasses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public final class SerializeSubclassesExample {
+	public static class ListHolder {
+		@Serialize(order = 0)
+		@SerializeSubclasses(extraSubclassesId = "list", path = 0)
+		public List<Object> list = new ArrayList<>();
+	}
+
+	public static void main(String[] arg) {
+		List<Object> list1 = asList(123, 456L, "text", false, "content", true);
+		serializeList(list1, Integer.class, Long.class, String.class, Boolean.class);
+
+		System.out.println();
+
+		List<Object> list2 = asList((byte) 1, 12.34, 4353.323f, 'X');
+		serializeList(list2, Byte.class, Double.class, Float.class, String.class, Character.class);
+	}
+
+	private static void serializeList(List<Object> list, Class<?>... subClasses) {
+		ListHolder holder = new ListHolder();
+
+		holder.list.addAll(list);
+
+		BinarySerializer<ListHolder> serializer = SerializerBuilder.create()
+				.withSubclasses("list", subClasses)
+				.build(ListHolder.class);
+
+		byte[] buffer = new byte[1024];
+		serializer.encode(buffer, 0, holder);
+		ListHolder decoded = serializer.decode(buffer, 0);
+
+		printLists(list, decoded.list);
+	}
+
+	private static final String FORMAT_PATTERN = "%-10s%-10s%n";
+
+	private static void printLists(List<Object> list, List<Object> decodedList) {
+		assert list.size() == decodedList.size();
+
+		System.out.printf(FORMAT_PATTERN, "ORIGINAL", "DECODED");
+		System.out.printf(FORMAT_PATTERN, "--------", "--------");
+		for (int i = 0; i < list.size(); i++) {
+			Object element = list.get(i);
+			Object decodedElement = decodedList.get(i);
+			System.out.printf(FORMAT_PATTERN, element, decodedElement);
+		}
+	}
+}
