33af6f8947c70c078e6e76ead35452f97ddd26d6
Eduard Vasinskyi <evasinskyi@datakernel.io>
2021-06-02 16:28:01 +0300

    Bump to 4.4-SNAPSHOT

--- core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
+++ core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
@@ -28,7 +28,7 @@ import static io.activej.http.HttpHeaders.*;
  */
 @FunctionalInterface
 public interface HttpExceptionFormatter {
-	String ACTIVEJ_VERSION = "4.3-SNAPSHOT";
+	String ACTIVEJ_VERSION = "4.4-SNAPSHOT";
 
 	@Language("HTML")
 	String HTTP_ERROR_HTML = "<!doctype html>" +
