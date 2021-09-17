b49efec73455db1387a106c1c43f60416988277a
Dmitry Volvach <dmitry@activej.io>
2021-09-17 19:17:45 +0300

    Bump to 5.0-rc2

--- core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
+++ core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
@@ -28,7 +28,7 @@ import static io.activej.http.HttpHeaders.*;
  */
 @FunctionalInterface
 public interface HttpExceptionFormatter {
-	String ACTIVEJ_VERSION = "5.0-SNAPSHOT";
+	String ACTIVEJ_VERSION = "5.0-rc2";
 
 	@Language("HTML")
 	String HTTP_ERROR_HTML = "<!doctype html>" +
