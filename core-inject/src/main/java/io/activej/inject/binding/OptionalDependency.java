2d0539169f132aa02e89c155618a2098eb70d5ee
Eduard Vasinskyi <evasinskyi@datakernel.io>
2021-10-25 14:30:05 +0300

    Add OptionalDependency

---
+++ core-inject/src/main/java/io/activej/inject/binding/OptionalDependency.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2020 ActiveJ LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.activej.inject.binding;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.NoSuchElementException;
+
+public final class OptionalDependency<T> {
+	private final @Nullable T value;
+
+	private OptionalDependency(@Nullable T value) {
+		this.value = value;
+	}
+
+	public static <T> OptionalDependency<T> of(@NotNull T value) {
+		return new OptionalDependency<>(value);
+	}
+
+	public static <T> OptionalDependency<T> empty() {
+		return new OptionalDependency<>(null);
+	}
+
+	public boolean isPresent() {
+		return value != null;
+	}
+
+	public T get() {
+		if (value == null) {
+			throw new NoSuchElementException();
+		}
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		OptionalDependency<?> that = (OptionalDependency<?>) o;
+
+		return value != null ? value.equals(that.value) : that.value == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return value != null ? value.hashCode() : 0;
+	}
+}
