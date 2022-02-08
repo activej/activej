c31e5524edc1ebec618ae6ec4149d94f104dddfe
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-02-08 13:20:39 +0200

    Add TYPE_USE target to @Local annotation

--- launchers/crdt/src/main/java/io/activej/launchers/crdt/Local.java
+++ launchers/crdt/src/main/java/io/activej/launchers/crdt/Local.java
@@ -21,12 +21,11 @@ import io.activej.inject.annotation.QualifierAnnotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @QualifierAnnotation
-@Target({PARAMETER, METHOD})
+@Target({PARAMETER, METHOD, TYPE_USE})
 @Retention(RUNTIME)
 public @interface Local {
 }
