d7adb2f883048933a9c8bde1cc588cb6fa69a504
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-05-03 14:32:03 +0300

    Add tests for StreamRateLimiter

---
+++ core-datastream/src/test/java/io/activej/datastream/processor/StreamRateLimiterTest.java
@@ -0,0 +1,87 @@
+package io.activej.datastream.processor;
+
+import io.activej.common.ref.RefLong;
+import io.activej.datastream.StreamConsumer;
+import io.activej.datastream.StreamSupplier;
+import io.activej.eventloop.Eventloop;
+import io.activej.test.rules.EventloopRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static io.activej.promise.TestUtils.await;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class StreamRateLimiterTest {
+
+	@ClassRule
+	public static final EventloopRule eventloopRule = new EventloopRule();
+
+	@Test
+	public void testEmpty() {
+		Eventloop eventloop = Eventloop.getCurrentEventloop();
+		StreamRateLimiter<Integer> limiter = StreamRateLimiter.create(eventloop, 100);
+
+		List<Integer> expected = IntStream.range(0, 200)
+				.boxed().collect(toList());
+		List<Integer> actual = new ArrayList<>();
+
+		RefLong passed = new RefLong(eventloop.currentTimeMillis());
+		await(StreamSupplier.ofIterable(expected)
+				.transformWith(limiter)
+				.streamTo(StreamConsumer.ofConsumer(actual::add))
+				.whenResult(() -> passed.value = eventloop.currentTimeMillis() - passed.value));
+
+
+		assertEquals(expected, actual);
+		assertTrue(passed.value > 2_000);
+	}
+
+	@Test
+	public void testHalfFull() {
+		Eventloop eventloop = Eventloop.getCurrentEventloop();
+		StreamRateLimiter<Integer> limiter = StreamRateLimiter.<Integer>create(eventloop, 100)
+				.withInitialTokens(100L);
+
+		List<Integer> expected = IntStream.range(0, 200)
+				.boxed().collect(toList());
+		List<Integer> actual = new ArrayList<>();
+
+		RefLong passed = new RefLong(eventloop.currentTimeMillis());
+		await(StreamSupplier.ofIterable(expected)
+				.transformWith(limiter)
+				.streamTo(StreamConsumer.ofConsumer(actual::add))
+				.whenResult(() -> passed.value = eventloop.currentTimeMillis() - passed.value));
+
+
+		assertEquals(expected, actual);
+		assertTrue(passed.value > 1_000 && passed.value < 2_000);
+	}
+
+	@Test
+	public void testFull() {
+		Eventloop eventloop = Eventloop.getCurrentEventloop();
+		StreamRateLimiter<Integer> limiter = StreamRateLimiter.<Integer>create(eventloop, 100)
+				.withInitialTokens(200L);
+
+		List<Integer> expected = IntStream.range(0, 200)
+				.boxed().collect(toList());
+		List<Integer> actual = new ArrayList<>();
+
+		RefLong passed = new RefLong(eventloop.currentTimeMillis());
+		await(StreamSupplier.ofIterable(expected)
+				.transformWith(limiter)
+				.streamTo(StreamConsumer.ofConsumer(actual::add))
+				.whenResult(() -> passed.value = eventloop.currentTimeMillis() - passed.value));
+
+
+		assertEquals(expected, actual);
+		assertTrue(passed.value < 1_000);
+	}
+
+}
