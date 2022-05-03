fd2629fa161a5b40bd659044c0537487322e841b
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-05-03 13:12:41 +0300

    Add channel rate limiter

---
+++ core-csp/src/main/java/io/activej/csp/process/ChannelRateLimiter.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2020 ActiveJ LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.activej.csp.process;
+
+import io.activej.bytebuf.ByteBuf;
+import io.activej.common.initializer.WithInitializer;
+import io.activej.eventloop.Eventloop;
+import io.activej.eventloop.schedule.ScheduledRunnable;
+import io.activej.promise.Promise;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static io.activej.common.Utils.nullify;
+
+public final class ChannelRateLimiter<T> extends AbstractChannelTransformer<ChannelRateLimiter<T>, T, T>
+		implements WithInitializer<ChannelRateLimiter<T>> {
+	private final Eventloop eventloop;
+	private final long refillRatePerSecond;
+
+	private long tokens;
+	private long lastRefillTimestamp;
+	private Tokenizer<T> tokenizer = $ -> 1;
+
+	private @Nullable ScheduledRunnable scheduledRunnable;
+
+	private ChannelRateLimiter(Eventloop eventloop, long refillRatePerSecond) {
+		this.eventloop = eventloop;
+		this.refillRatePerSecond = refillRatePerSecond;
+		this.lastRefillTimestamp = eventloop.currentTimeMillis();
+	}
+
+	public static <T> ChannelRateLimiter<T> create(Eventloop eventloop, long refillRatePerSecond) {
+		return new ChannelRateLimiter<>(eventloop, refillRatePerSecond);
+	}
+
+	public ChannelRateLimiter<T> withInitialTokens(long initialTokens) {
+		this.tokens = initialTokens;
+		return this;
+	}
+
+	public ChannelRateLimiter<T> withTokenizer(Tokenizer<T> tokenizer) {
+		this.tokenizer = tokenizer;
+		return this;
+	}
+
+	@Override
+	protected @NotNull Promise<Void> onItem(T item) {
+		scheduledRunnable = null;
+
+		refill();
+
+		long itemTokens = tokenizer.getTokens(item);
+		if (itemTokens <= tokens) {
+			tokens -= itemTokens;
+			return send(item);
+		}
+
+		return Promise.ofCallback(cb -> scheduledRunnable = eventloop.delay(calculateDelay(itemTokens), () -> onItem(item).whenComplete(cb::accept)));
+	}
+
+	private void refill() {
+		long timestamp = eventloop.currentTimeMillis();
+		long passedMillis = timestamp - lastRefillTimestamp;
+
+		tokens += (long) (passedMillis / 1000.0d * refillRatePerSecond);
+		lastRefillTimestamp = timestamp;
+	}
+
+	private long calculateDelay(long itemTokens) {
+		long missing = itemTokens - tokens;
+		assert missing > 0;
+
+		double secondsToRefill = (double) missing / refillRatePerSecond;
+
+		return Math.max(1, (long) (secondsToRefill * 1000));
+	}
+
+	@Override
+	protected void onCleanup() {
+		scheduledRunnable = nullify(scheduledRunnable, ScheduledRunnable::cancel);
+	}
+
+	public interface Tokenizer<T> {
+		long getTokens(T item);
+
+		static Tokenizer<ByteBuf> forByteBufs() {
+			return ByteBuf::readRemaining;
+		}
+	}
+}
