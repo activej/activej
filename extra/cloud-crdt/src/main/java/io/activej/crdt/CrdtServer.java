814f9e3b2c238186966064bce646e898963dfdf0
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-05-04 12:30:52 +0300

    Add JMX stats for CrdtServer

--- extra/cloud-crdt/src/main/java/io/activej/crdt/CrdtServer.java
+++ extra/cloud-crdt/src/main/java/io/activej/crdt/CrdtServer.java
@@ -24,23 +24,35 @@ import io.activej.crdt.CrdtMessagingProto.Version;
 import io.activej.crdt.storage.CrdtStorage;
 import io.activej.crdt.util.CrdtDataSerializer;
 import io.activej.csp.binary.ByteBufsCodec;
+import io.activej.csp.net.Messaging;
 import io.activej.csp.net.MessagingWithBinaryStreaming;
 import io.activej.datastream.StreamConsumer;
 import io.activej.datastream.csp.ChannelDeserializer;
 import io.activej.datastream.csp.ChannelSerializer;
+import io.activej.datastream.stats.StreamStats;
+import io.activej.datastream.stats.StreamStatsBasic;
+import io.activej.datastream.stats.StreamStatsDetailed;
 import io.activej.eventloop.Eventloop;
+import io.activej.jmx.api.attribute.JmxAttribute;
+import io.activej.jmx.api.attribute.JmxOperation;
 import io.activej.net.AbstractServer;
 import io.activej.net.socket.tcp.AsyncTcpSocket;
 import io.activej.promise.Promise;
+import io.activej.promise.jmx.PromiseStats;
 import io.activej.serializer.BinarySerializer;
 
 import java.net.InetAddress;
+import java.time.Duration;
 import java.util.function.Function;
 
+import static io.activej.async.util.LogUtils.Level.TRACE;
+import static io.activej.async.util.LogUtils.thisMethod;
+import static io.activej.async.util.LogUtils.toLogger;
 import static io.activej.crdt.CrdtMessagingProto.CrdtRequest.RequestCase.TAKE_ACK;
 import static io.activej.crdt.CrdtMessagingProto.CrdtResponse.ResponseCase.*;
 import static io.activej.fs.util.ProtobufUtils.codec;
 
+@SuppressWarnings("rawtypes")
 public final class CrdtServer<K extends Comparable<K>, S> extends AbstractServer<CrdtServer<K, S>> {
 	public static final Version VERSION = Version.newBuilder().setMajor(1).setMinor(0).build();
 
@@ -53,6 +65,30 @@ public final class CrdtServer<K extends Comparable<K>, S> extends AbstractServer
 	private final CrdtStorage<K, S> storage;
 	private final CrdtDataSerializer<K, S> serializer;
 	private final BinarySerializer<CrdtTombstone<K>> tombstoneSerializer;
+
+	// region JMX
+	private boolean detailedStats;
+
+	private final StreamStatsBasic<CrdtData<K, S>> uploadStats = StreamStats.basic();
+	private final StreamStatsDetailed<CrdtData<K, S>> uploadStatsDetailed = StreamStats.detailed();
+	private final StreamStatsBasic<CrdtData<K, S>> downloadStats = StreamStats.basic();
+	private final StreamStatsDetailed<CrdtData<K, S>> downloadStatsDetailed = StreamStats.detailed();
+	private final StreamStatsBasic<CrdtData<K, S>> takeStats = StreamStats.basic();
+	private final StreamStatsDetailed<CrdtData<K, S>> takeStatsDetailed = StreamStats.detailed();
+	private final StreamStatsBasic<CrdtTombstone<K>> removeStats = StreamStats.basic();
+	private final StreamStatsDetailed<CrdtTombstone<K>> removeStatsDetailed = StreamStats.detailed();
+
+	private final PromiseStats handshakePromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats downloadBeginPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats downloadFinishedPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats uploadBeginPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats uploadFinishedPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats removeBeginPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats removeFinishedPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats takeBeginPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats takeFinishedPromise = PromiseStats.create(Duration.ofMinutes(5));
+	private final PromiseStats pingPromise = PromiseStats.create(Duration.ofMinutes(5));
+	// endregion
 
 	private CrdtServer(Eventloop eventloop, CrdtStorage<K, S> storage, CrdtDataSerializer<K, S> serializer) {
 		super(eventloop);
@@ -84,51 +120,20 @@ public final class CrdtServer<K extends Comparable<K>, S> extends AbstractServer
 					if (!handshakeMsg.hasHandshake()) {
 						return Promise.ofException(new CrdtException("Handshake expected"));
 					}
-					CrdtResponse handshakeResponse = CrdtResponse.newBuilder()
-							.setHandshake(handshakeHandler.apply(handshakeMsg.getHandshake()))
-							.build();
-					return messaging.send(handshakeResponse)
+					return handshake(messaging, handshakeMsg.getHandshake())
 							.then(messaging::receive)
 							.then(msg -> {
 								switch (msg.getRequestCase()) {
 									case DOWNLOAD:
-										return storage.download((msg.getDownload()).getToken())
-												.whenResult(() -> messaging.send(response(DOWNLOAD_STARTED)))
-												.then(supplier -> supplier
-														.transformWith(ChannelSerializer.create(serializer))
-														.streamTo(messaging.sendBinaryStream()));
+										return download(messaging, msg.getDownload());
 									case UPLOAD:
-										return messaging.receiveBinaryStream()
-												.transformWith(ChannelDeserializer.create(serializer))
-												.streamTo(StreamConsumer.ofPromise(storage.upload()))
-												.then(() -> messaging.send(response(UPLOAD_ACK)))
-												.then(messaging::sendEndOfStream)
-												.whenResult(messaging::close);
+										return upload(messaging, msg.getUpload());
 									case REMOVE:
-										return messaging.receiveBinaryStream()
-												.transformWith(ChannelDeserializer.create(tombstoneSerializer))
-												.streamTo(StreamConsumer.ofPromise(storage.remove()))
-												.then(() -> messaging.send(response(REMOVE_ACK)))
-												.then(messaging::sendEndOfStream)
-												.whenResult(messaging::close);
+										return remove(messaging, msg.getRemove());
 									case PING:
-										return messaging.send(response(PONG))
-												.then(messaging::sendEndOfStream)
-												.whenResult(messaging::close);
+										return ping(messaging, msg.getPing());
 									case TAKE:
-										return storage.take()
-												.whenResult(() -> messaging.send(response(TAKE_STARTED)))
-												.then(supplier -> supplier
-														.transformWith(ChannelSerializer.create(serializer))
-														.streamTo(messaging.sendBinaryStream()
-																.withAcknowledgement(ack -> ack
-																		.then(() -> messaging.receive()
-																				.then(takeAck -> {
-																					if (!takeAck.hasTakeAck()) {
-																						return Promise.ofException(new CrdtException("Received message " + takeAck + " instead of " + TAKE_ACK));
-																					}
-																					return Promise.complete();
-																				})))));
+										return take(messaging, msg.getTake());
 									case HANDSHAKE:
 										return Promise.ofException(new CrdtException("Handshake was already performed"));
 									case REQUEST_NOT_SET:
@@ -144,6 +149,82 @@ public final class CrdtServer<K extends Comparable<K>, S> extends AbstractServer
 							.then(messaging::sendEndOfStream)
 							.whenResult(messaging::close);
 				});
+	}
+
+	private Promise<Void> handshake(Messaging<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Handshake handshake) {
+		CrdtResponse handShakeResponse = CrdtResponse.newBuilder()
+				.setHandshake(handshakeHandler.apply(handshake))
+				.build();
+
+		return messaging.send(handShakeResponse)
+				.whenComplete(handshakePromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, handshake, this));
+	}
+
+	private Promise<Void> take(MessagingWithBinaryStreaming<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Take take) {
+		return storage.take()
+				.whenComplete(takeBeginPromise.recordStats())
+				.whenResult(() -> messaging.send(response(TAKE_STARTED)))
+				.then(supplier -> supplier
+						.transformWith(detailedStats ? takeStatsDetailed : takeStats)
+						.transformWith(ChannelSerializer.create(serializer))
+						.streamTo(messaging.sendBinaryStream()
+								.withAcknowledgement(ack -> ack
+										.then(() -> messaging.receive()
+												.then(takeAck -> {
+													if (!takeAck.hasTakeAck()) {
+														return Promise.ofException(new CrdtException("Received message " + takeAck + " instead of " + TAKE_ACK));
+													}
+													return Promise.complete();
+												})))))
+				.whenComplete(takeFinishedPromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, take, this));
+	}
+
+	private Promise<Void> ping(MessagingWithBinaryStreaming<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Ping ping) {
+		return messaging.send(response(PONG))
+				.then(messaging::sendEndOfStream)
+				.whenResult(messaging::close)
+				.whenComplete(pingPromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, ping, this));
+	}
+
+	private Promise<Void> remove(MessagingWithBinaryStreaming<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Remove remove) {
+		return messaging.receiveBinaryStream()
+				.transformWith(ChannelDeserializer.create(tombstoneSerializer))
+				.streamTo(StreamConsumer.ofPromise(storage.remove()
+						.map(consumer -> consumer.transformWith(detailedStats ? removeStatsDetailed : removeStats))
+						.whenComplete(removeBeginPromise.recordStats())))
+				.then(() -> messaging.send(response(REMOVE_ACK)))
+				.then(messaging::sendEndOfStream)
+				.whenResult(messaging::close)
+				.whenComplete(removeFinishedPromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, remove, this));
+	}
+
+	private Promise<Void> upload(MessagingWithBinaryStreaming<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Upload upload) {
+		return messaging.receiveBinaryStream()
+				.transformWith(ChannelDeserializer.create(serializer))
+				.streamTo(StreamConsumer.ofPromise(storage.upload()
+						.map(consumer -> consumer.transformWith(detailedStats ? uploadStatsDetailed : uploadStats))
+						.whenComplete(uploadBeginPromise.recordStats())))
+				.then(() -> messaging.send(response(UPLOAD_ACK)))
+				.then(messaging::sendEndOfStream)
+				.whenResult(messaging::close)
+				.whenComplete(uploadFinishedPromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, upload, this));
+	}
+
+	private Promise<Void> download(MessagingWithBinaryStreaming<CrdtRequest, CrdtResponse> messaging, CrdtRequest.Download download) {
+		return storage.download(download.getToken())
+				.map(consumer -> consumer.transformWith(detailedStats ? downloadStatsDetailed : downloadStats))
+				.whenComplete(downloadBeginPromise.recordStats())
+				.whenResult(() -> messaging.send(response(DOWNLOAD_STARTED)))
+				.then(supplier -> supplier
+						.transformWith(ChannelSerializer.create(serializer))
+						.streamTo(messaging.sendBinaryStream()))
+				.whenComplete(downloadFinishedPromise.recordStats())
+				.whenComplete(toLogger(logger, TRACE, thisMethod(), messaging, download, this));
 	}
 
 	private static CrdtResponse response(ResponseCase responseCase) {
@@ -170,4 +251,111 @@ public final class CrdtServer<K extends Comparable<K>, S> extends AbstractServer
 						.setMessage(exception.getClass().getSimpleName() + ": " + exception.getMessage()))
 				.build();
 	}
+
+	// region JMX
+	@JmxAttribute
+	public boolean isDetailedStats() {
+		return detailedStats;
+	}
+
+	@JmxOperation
+	public void startDetailedMonitoring() {
+		detailedStats = true;
+	}
+
+	@JmxOperation
+	public void stopDetailedMonitoring() {
+		detailedStats = false;
+	}
+
+	@JmxAttribute
+	public StreamStatsBasic getUploadStats() {
+		return uploadStats;
+	}
+
+	@JmxAttribute
+	public StreamStatsDetailed getUploadStatsDetailed() {
+		return uploadStatsDetailed;
+	}
+
+	@JmxAttribute
+	public StreamStatsBasic getDownloadStats() {
+		return downloadStats;
+	}
+
+	@JmxAttribute
+	public StreamStatsDetailed getDownloadStatsDetailed() {
+		return downloadStatsDetailed;
+	}
+
+	@JmxAttribute
+	public StreamStatsBasic getTakeStats() {
+		return takeStats;
+	}
+
+	@JmxAttribute
+	public StreamStatsDetailed getTakeStatsDetailed() {
+		return takeStatsDetailed;
+	}
+
+	@JmxAttribute
+	public StreamStatsBasic getRemoveStats() {
+		return removeStats;
+	}
+
+	@JmxAttribute
+	public StreamStatsDetailed getRemoveStatsDetailed() {
+		return removeStatsDetailed;
+	}
+
+	@JmxAttribute
+	public PromiseStats getHandshakePromise() {
+		return handshakePromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getDownloadBeginPromise() {
+		return downloadBeginPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getDownloadFinishedPromise() {
+		return downloadFinishedPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getUploadBeginPromise() {
+		return uploadBeginPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getUploadFinishedPromise() {
+		return uploadFinishedPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getRemoveBeginPromise() {
+		return removeBeginPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getRemoveFinishedPromise() {
+		return removeFinishedPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getTakeBeginPromise() {
+		return takeBeginPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getTakeFinishedPromise() {
+		return takeFinishedPromise;
+	}
+
+	@JmxAttribute
+	public PromiseStats getPingPromise() {
+		return pingPromise;
+	}
+	// endregion
 }
