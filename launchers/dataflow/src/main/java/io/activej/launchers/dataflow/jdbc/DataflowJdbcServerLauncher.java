519ac7722c2a9ddc48b32d97cd8452eacaf066c6
Eduard Vasinskyi <evasinskyi@datakernel.io>
2022-09-29 16:14:27 +0300

    Add DataflowJdbcServerLauncher

---
+++ launchers/dataflow/src/main/java/io/activej/launchers/dataflow/jdbc/DataflowJdbcServerLauncher.java
@@ -0,0 +1,73 @@
+package io.activej.launchers.dataflow.jdbc;
+
+import io.activej.config.Config;
+import io.activej.dataflow.SqlDataflow;
+import io.activej.dataflow.calcite.inject.CalciteClientModule;
+import io.activej.datastream.StreamConsumer;
+import io.activej.eventloop.Eventloop;
+import io.activej.inject.annotation.Inject;
+import io.activej.inject.annotation.Provides;
+import io.activej.inject.module.AbstractModule;
+import io.activej.inject.module.Module;
+import io.activej.inject.module.Modules;
+import io.activej.launchers.dataflow.DataflowClientLauncher;
+
+import java.io.IOException;
+import java.nio.file.Files;
+
+public abstract class DataflowJdbcServerLauncher extends DataflowClientLauncher {
+	public static final int DEFAULT_JDBC_SERVER_PORT = 3387;
+	public static final String PROPERTIES_FILE = "dataflow-jdbc-server.properties";
+
+	@Inject
+	protected Eventloop eventloop;
+
+	@Inject
+	protected SqlDataflow sqlDataflow;
+
+	/**
+	 * Override this method to supply your dataflow schema.
+	 * Bindings from this module override any other bindings.
+	 */
+	protected Module getDataflowSchemaModule() {
+		return Module.empty();
+	}
+
+	@Override
+	protected final Module getBusinessLogicModule() {
+		return Modules.combine(
+				CalciteClientModule.create(),
+				DataflowJdbcServerModule.create()
+		);
+	}
+
+	@Override
+	protected final Module getOverrideModule() {
+		return Modules.combine(
+				getDataflowSchemaModule(),
+				new AbstractModule() {
+					@Provides
+					Config config() throws IOException {
+						return Config.create()
+								.with("dataflow.jdbc.server.port", String.valueOf(DEFAULT_JDBC_SERVER_PORT))
+								.with("dataflow.secondaryBufferPath", Files.createTempDirectory("secondaryBufferPath").toString())
+								.overrideWith(Config.ofClassPathProperties(PROPERTIES_FILE, true))
+								.overrideWith(Config.ofProperties(System.getProperties()).getChild("config"));
+					}
+				});
+	}
+
+	@Override
+	protected final void onStart() throws Exception {
+		eventloop.submit(() -> sqlDataflow.query("SELECT 1")
+						.then(supplier -> supplier.streamTo(StreamConsumer.skip())))
+				.get();
+
+		System.out.println("Connection to partitions established");
+	}
+
+	@Override
+	protected final void run() throws Exception {
+		awaitShutdown();
+	}
+}
