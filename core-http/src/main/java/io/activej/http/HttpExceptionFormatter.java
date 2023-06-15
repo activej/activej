e38d6d338c5dcd8c89a1eaa2541237fe07500b96
Dmitry Volvach <dmitry@activej.io>
2023-06-15 17:55:07 +0300

    Bump to 6.0-beta1

--- core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
+++ core-http/src/main/java/io/activej/http/HttpExceptionFormatter.java
@@ -28,7 +28,7 @@ import static io.activej.http.HttpHeaders.*;
  */
 @FunctionalInterface
 public interface HttpExceptionFormatter {
-	String ACTIVEJ_VERSION = "6.0-SNAPSHOT";
+	String ACTIVEJ_VERSION = "6.0-beta1";
 
 	@Language("HTML")
 	String HTTP_ERROR_HTML = """
