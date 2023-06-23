a0025e039d6b4e8524b16d1d2026c717b615c2d1
Eduard Vasinskyi <evasinskyi@datakernel.io>
2023-06-23 17:12:44 +0300

    Add tests for malformed JSONs

--- extra/cloud-lsmt-cube/src/test/java/io/activej/cube/http/JsonTest.java
+++ extra/cloud-lsmt-cube/src/test/java/io/activej/cube/http/JsonTest.java
@@ -1,19 +1,22 @@
 package io.activej.cube.http;
 
-import com.dslplatform.json.JsonReader;
+import com.dslplatform.json.JsonReader.ReadObject;
 import com.dslplatform.json.StringConverter;
 import io.activej.common.exception.MalformedDataException;
+import io.activej.cube.bean.TestPubRequest.TestEnum;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
+import static io.activej.cube.Utils.CUBE_DSL_JSON;
 import static io.activej.cube.Utils.fromJson;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class JsonTest {
 
 	@Test
 	public void errorOnTrailingData() throws MalformedDataException {
-		JsonReader.ReadObject<String> reader = StringConverter.READER;
+		ReadObject<String> reader = StringConverter.READER;
 		String stringJson = "\"string\"";
 		String string = fromJson(reader, stringJson);
 		assertEquals("string", string);
@@ -24,6 +27,34 @@ public class JsonTest {
 			fail();
 		} catch (MalformedDataException e){
 			assertEquals(e.getMessage(), "Unexpected JSON data: " + moreData);
+		}
+	}
+
+	@Test
+	public void errorOnMalformedLocalDate() {
+		ReadObject<LocalDate> reader = CUBE_DSL_JSON.tryFindReader(LocalDate.class);
+		assertNotNull(reader);
+		try {
+			fromJson(reader, "\"INVALID DATE\"");
+			fail();
+		} catch (MalformedDataException e){
+			assertEquals(e.getMessage(), "com.dslplatform.json.ParsingException: " +
+					"Text 'INVALID DATE' could not be parsed at index 0. " +
+					"Found \" at position: 14, following: `\"INVALID DATE\"`");
+		}
+	}
+
+	@Test
+	public void errorOnMalformedEnum() {
+		ReadObject<TestEnum> reader = CUBE_DSL_JSON.tryFindReader(TestEnum.class);
+		assertNotNull(reader);
+		try {
+			fromJson(reader, "\"INVALID ENUM\"");
+			fail();
+		} catch (MalformedDataException e){
+			assertEquals(e.getMessage(), "com.dslplatform.json.ParsingException: " +
+					"No enum constant io.activej.cube.bean.TestPubRequest.TestEnum.INVALID ENUM. " +
+					"Found \" at position: 14, following: `\"INVALID ENUM\"`");
 		}
 	}
 }
