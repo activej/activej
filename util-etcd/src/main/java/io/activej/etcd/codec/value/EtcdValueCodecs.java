ddc29504cb2380eaea6512b6a1f65cd32d810c9d
Dmitry Volvach <dmitry@activej.io>
2023-09-21 23:02:16 +0300

    Implement String numeric etcd codecs

--- util-etcd/src/main/java/io/activej/etcd/codec/value/EtcdValueCodecs.java
+++ util-etcd/src/main/java/io/activej/etcd/codec/value/EtcdValueCodecs.java
@@ -24,6 +24,42 @@ public class EtcdValueCodecs {
 		};
 	}
 
+	public static EtcdValueCodec<Integer> ofIntegerString() {
+		return new EtcdValueCodec<>() {
+			@Override
+			public ByteSequence encodeValue(Integer value) {
+				return byteSequenceFrom(Integer.toString(value));
+			}
+
+			@Override
+			public Integer decodeValue(ByteSequence byteSequence) throws MalformedDataException {
+				try {
+					return Integer.parseInt(byteSequence.toString());
+				} catch (NumberFormatException e) {
+					throw new MalformedDataException(e);
+				}
+			}
+		};
+	}
+
+	public static EtcdValueCodec<Long> ofLongString() {
+		return new EtcdValueCodec<>() {
+			@Override
+			public ByteSequence encodeValue(Long value) {
+				return byteSequenceFrom(Long.toString(value));
+			}
+
+			@Override
+			public Long decodeValue(ByteSequence byteSequence) throws MalformedDataException {
+				try {
+					return Long.parseLong(byteSequence.toString());
+				} catch (NumberFormatException e) {
+					throw new MalformedDataException(e);
+				}
+			}
+		};
+	}
+
 	public static <T, R> EtcdValueCodec<R> transform(EtcdValueCodec<T> codec, Function<R, T> encodeFn, DecoderFunction<T, R> decodeFn) {
 		return new EtcdValueCodec<>() {
 			@Override
