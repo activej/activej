b7a50d262704acda61befeeb4c6d917743d8ad58
Dmitry Volvach <dmitry@activej.io>
2023-11-24 22:07:35 +0100

    Remove UIKernel dependency

--- examples/tutorials/uikernel-integration/front/webapp/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var ReactDOM = require('react-dom');
-var React = require('react');
-var AppComponent = require('./components/App');
-
-ReactDOM.render(<AppComponent/>, document.getElementById('example'));
--- examples/tutorials/uikernel-integration/src/main/java/Gender.java
+++
@@ -1,3 +0,0 @@
-public enum Gender {
-	MALE, FEMALE
-}
