7d60ef8b3b895bebdbb6715641640beae614166d
Eduard Vasinskyi <evasinskyi@datakernel.io>
2024-04-17 13:42:44 +0300

    Add test for inject annotation in singleton scope

--- boot-workers/src/test/java/io/activej/worker/WorkerPoolTest.java
+++ boot-workers/src/test/java/io/activej/worker/WorkerPoolTest.java
@@ -2,17 +2,22 @@ package io.activej.worker;
 
 import io.activej.common.ref.RefInt;
 import io.activej.inject.Injector;
+import io.activej.inject.Key;
+import io.activej.inject.annotation.Inject;
 import io.activej.inject.module.AbstractModule;
+import io.activej.inject.module.ModuleBuilder;
 import io.activej.worker.annotation.Worker;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class WorkerPoolTest {
 	private WorkerPool first;
@@ -54,5 +59,34 @@ public class WorkerPoolTest {
 		actual.addAll(second.getInstances(String.class).getList());
 		Set<String> expected = IntStream.range(0, 14).mapToObj(i -> "String: " + i).collect(toSet());
 		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void injectAnnotationInSingletonScope() {
+		Injector injector = Injector.of(
+			WorkerPoolModule.create(),
+			ModuleBuilder.create()
+				.bind(WorkerPool.class).to(workerPools -> workerPools.createPool(4), WorkerPools.class)
+				.bind(Id.class)
+				.bind(new Key<WorkerPool.Instances<Integer>>() {})
+				.bind(Integer.class).to(id -> id.id, Id.class).in(Worker.class)
+				.build()
+		);
+
+		WorkerPool.Instances<Integer> instance = injector.getInstance(new Key<>() {});
+		List<Integer> list = instance.getList();
+
+		assertEquals(4, list.size());
+		assertTrue(list.stream().allMatch(id -> id == 1));
+	}
+
+	public static final class Id {
+		private static int counter = 0;
+
+		private final int id = ++counter;
+
+		@Inject
+		public Id() {
+		}
 	}
 }
