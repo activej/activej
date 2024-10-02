8e8a68f059859b4c1d5394736078452d3e19e371
Eduard Vasinskyi <evasinskyi@datakernel.io>
2024-10-02 15:51:16 +0300

    Print stats in ByteBuf queue stress test

--- core-bytebuf/src/test/java/io/activej/bytebuf/ByteBufConcurrentQueueStressTest.java
+++ core-bytebuf/src/test/java/io/activej/bytebuf/ByteBufConcurrentQueueStressTest.java
@@ -7,35 +7,43 @@ public class ByteBufConcurrentQueueStressTest {
 	private static final byte[] bytes = new byte[1];
 
 	public static void main(String[] args) throws InterruptedException {
-		for (int i = 0; i < 10000; i++) {
-			round();
-		}
+		round();
 	}
 
 	private static void round() throws InterruptedException {
 		ByteBufConcurrentQueue queue = new ByteBufConcurrentQueue();
-		List<Thread> threads = new ArrayList<>();
-		for (int i = 0; i < 20; i++) {
-			int finalI = i;
-			Thread thread = new Thread(() -> {
-				for (int j = 0; j < 10000; j++) {
-					ByteBuf buf = queue.poll();
-					if (buf == null) {
-						buf = ByteBuf.wrapForReading(bytes);
+		for (int x = 0; x < 100; x++) {
+			List<Thread> threads = new ArrayList<>();
+			for (int i = 0; i < 20; i++) {
+				int finalI = i;
+				Thread thread = new Thread(() -> {
+					for (int j = 0; j < 10_000; j++) {
+						ByteBuf buf = queue.poll();
+						if (buf == null) {
+							buf = ByteBuf.wrapForReading(bytes);
+						}
+						buf.pos = finalI;
+						if (buf.pos != finalI) throw new AssertionError();
+						queue.offer(buf);
 					}
-					buf.pos = finalI;
-					if (buf.pos != finalI) throw new AssertionError();
-					queue.offer(buf);
-				}
-			});
-			threads.add(thread);
+				});
+				threads.add(thread);
+			}
+			for (Thread thread : threads) {
+				thread.start();
+			}
+			for (Thread thread : threads) {
+				thread.join();
+			}
+			System.out.println(queue);
+			int count = (x + 1) * 20 * 10_000;
+			System.out.println("Polled: " + count);
+			System.out.println("Offered: " + count);
+			System.out.println("Offer long path: " + queue.offerLongPath);
+			System.out.println("Poll long path: " + queue.pollLongPath);
+			System.out.println("Poll long path ops: " + queue.pollLongPathOps);
+			System.out.println("Thread yielded: " + queue.threadYielded);
+
 		}
-		for (Thread thread : threads) {
-			thread.start();
-		}
-		for (Thread thread : threads) {
-			thread.join();
-		}
-		System.out.println(queue);
 	}
 }
